898ed06aa16397b2ad5e681ada730950ebfaa583
ldy <807799686@qq.com>
2020-12-11 22:30:11 +0800

    token

--- token/app.js
+++ token/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+var cookieParser = require('cookie-parser');
 const Token = require('./modul/Token')
- app.post('/',(req,res)=>{
+app.use(cookieParser()) 
+app.post('/',(req,res)=>{
      let {username,password} = req.query
-      
      let id = '65F65DF8SD';
      let tokenKey = Token.en(id);
+     console.log(tokenKey);
      return res.send({
          msg:"操作成功",
          status:200,
@@ -15,14 +17,14 @@ const Token = require('./modul/Token')
 app.get('/',(req,res)=>{
     let {name}=req.query
     let tokenKey = Token.de(name);
+    console.log(req.cookies);
     return res.send({
         msg:"操作成功",
         status:200,
         tokenKey
     })
 })
-
-app.listen(3000,(err)=>{
+app.listen(3003,(err)=>{
     console.log('服务器启动成功');    
 })
 //1. 安装 jsonwebtoken npm i jsonwebtoken
